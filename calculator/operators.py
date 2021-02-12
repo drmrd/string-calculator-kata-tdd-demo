5887e6951a1bae210079a771b8bb385f042ef7a0
Dan Moore <dmoore@pillartechnology.com>
2021-02-12 14:21:31 -0500

    Raise an error when user provides more than two integers in input

--- calculator/operators.py
+++ calculator/operators.py
@@ -1,4 +1,11 @@
 def add(numbers):
     if numbers:
-        return sum(int(number) for number in numbers.split(','))
+        numbers = numbers.split(',')
+        if len(numbers) > 2:
+            raise ValueError('add expects at most two integers in its input string')
+        result = 0
+        for number in numbers:
+            result += int(number)
+        return result
+
     return 0
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,5 @@
+import pytest
+
 import calculator
 
 
@@ -23,3 +25,8 @@ def test_add_when_given_1_and_2_by_a_comma_returns_their_sum():
 
 def test_add_when_given_1_and_3_by_a_comma_returns_their_sum():
     assert calculator.add('1,3') == 4
+
+
+def test_that_add_fails_when_given_more_than_two_integers_in_its_input_string():
+    with pytest.raises(ValueError):
+        assert calculator.add('2,3,5')
