b0b59e48a04793ae167962c4617d4f2fce7884fe
Dan Moore <dmoore@pillartechnology.com>
2021-02-12 13:31:25 -0500

    Validate add returns 1 when given 1

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,3 +3,7 @@ import calculator
 
 def test_add_when_given_an_empty_string_returns_0():
     assert calculator.add('') == 0
+
+
+def test_add_when_given_1_as_a_string_returns_1():
+    assert calculator.add('1') == 1
