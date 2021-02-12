3e9049297dbb6419e6b725c24f630d118cdf2d17
Dan Moore <dmoore@pillartechnology.com>
2021-02-12 14:16:13 -0500

    Further simplify conditionals

--- calculator/operators.py
+++ calculator/operators.py
@@ -1,9 +1,4 @@
 def add(numbers):
     if numbers:
-        numbers = numbers.split(',')
-        result = 0
-        for number in numbers:
-            result += int(number)
-        return result
-
+        return sum(int(number) for number in numbers.split(','))
     return 0
