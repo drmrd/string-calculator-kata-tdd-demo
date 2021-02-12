32d570ecc5a3f4912f5fd1f1cf75f566651be299
Dan Moore <dmoore@pillartechnology.com>
2021-02-12 14:28:50 -0500

    Support summing more than two integers

--- calculator/operators.py
+++ calculator/operators.py
@@ -3,7 +3,4 @@ def add(numbers):
         return 0
 
     numbers = numbers.split(',')
-    if len(numbers) > 2:
-        raise ValueError('add expects at most two integers in its input string')
-
     return sum(int(number) for number in numbers)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -27,6 +27,5 @@ def test_add_when_given_1_and_3_by_a_comma_returns_their_sum():
     assert calculator.add('1,3') == 4
 
 
-def test_that_add_fails_when_given_more_than_two_integers_in_its_input_string():
-    with pytest.raises(ValueError):
-        assert calculator.add('2,3,5')
+def test_add_when_given_more_than_two_integers_returns_their_sum():
+    assert calculator.add('2,3,5') == 10
